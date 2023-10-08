8b113ac635ee317101871d7aa38734cdf8e55fce
Oliver Theabould <olivertheabould@Olivers-MBP-2.home>
2023-10-08 12:56:28 +0100

    fix invalid exports

--- src/handlers/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as createTodo } from "./createTodo";
-export { default as updateTodo } from "./updateTodo";
-export { default as getTodo } from "./getTodo";
-export { default as deleteTodo } from "./deleteTodo";
